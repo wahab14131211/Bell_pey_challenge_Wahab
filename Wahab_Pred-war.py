9a16002a95d5cee021acec46b72745af53ad697b
wahab14131211 <wahab14131211@users.noreply.github.com>
2019-12-02 00:05:21 -0500

    Update comments to expand on the strategy used

--- Wahab_Pred-war.py
+++ Wahab_Pred-war.py
@@ -10,16 +10,15 @@ def wahab_predwar (x,y,z,b,r):
     #if player is NOT confident in the accuracy of b, check if opponent has a strong tendency to cooperate or betray (ignore this check if game has just started, and b==0)
         #if z-y is larger than y-x, it would be better to betray, otherwise cooperate (since betray heavy players will be filtered out by this point, the player does not need to worry about the exact value of x)
 
-    if (r**1.1)>randint(1,10000):                   #(raise r to the power of 1.1 so that by round ~5000, the player is sufficently confident in the validity of b).
+    if (r**1.1)>randint(1,10000):                   #raise r to the power of 1.1 so that by round ~5000, the player is sufficently confident in the validity of b
         avg_tokens_lost= ((x*b) + (z-y)*(1-b))/2    #avg_tokens_lost is average # of tokens lost of player elects to cooperate.
         if avg_tokens_lost <= 1:
             return "cooperate"
         return "betray"
-    
+
     else:
         if b>0.85 or (b>0.0001 and b<0.15):
             return "betray"
         if (z-y)/(y-x)>= 1:
             return "betray"
         return "cooperate"
-        
